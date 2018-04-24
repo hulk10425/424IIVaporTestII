2cb96d05edb87d47be327617ebc19331ec9873cf
Hulk <>
2018-04-24 17:12:05 +0800

    sdfsdfdsf

--- Package.swift
+++ Package.swift
@@ -9,16 +9,17 @@ let package = Package(
         .executable(name: "Run", targets: ["Run"])
     ],
     dependencies: [
-//        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "3.0.0")),
-//        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
     ],
     targets: [
         .target(
             name: "App",
+            dependencies: ["Vapor", "FluentProvider"],
             exclude: ["Config", "Public", "Resources"]
         ),
         .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests")
+        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
     ]
 )
 
